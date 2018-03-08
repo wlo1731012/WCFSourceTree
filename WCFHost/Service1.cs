6e6b30c4511f6edb4ee38f2987c4cadc7dbe500c
Tom Su <tom.su@asml.com>
2018-03-08 12:20:45 +0800

    Test application configulation file_1803081220

---
+++ WCFHost/Service1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data;
+using System.IO;
+using System.ServiceModel.Channels;
+using System.Threading;
+
+namespace WCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class Service1 : IService1
+    {
+        private List<string> _userList = new List<string>();
+        private string _filePath = "";
+
+        public static Dictionary<string, ICallBackServices> DicHostSessionid = new Dictionary<string,ICallBackServices>();// Record client's session id and CallBack
+        public static Dictionary<string, string> DicHostUserid = new Dictionary<string,string>();// Record client's user name and session id
+
+        public static bool isNewUser = false;
+        public static bool isSendMessage = false;
+
+        public static Person SendMessenger;
+        public static string ReceiveMessenger;
+
+        public delegate void ListenerHandler_ReceiveFile(string clientName, string fileName, bool isChangeFileName);
+        public static event ListenerHandler_ReceiveFile listenerHandler_ReceiveFile = null;
+
+        public FileStream fileStream;
+
+        public Service1()
+        {
+            DicHostSessionid = new Dictionary<string, ICallBackServices>();
+            DicHostUserid = new Dictionary<string, string>();
+        }
+        /// <summary>
+        /// Register client's name and closing event
+        /// </summary>
+        /// <param name="userName">Client's userName</param>
+        /// 
+        public void Register(string userName)
+        {
+            ICallBackServices client = OperationContext.Current.GetCallbackChannel<ICallBackServices>();
+            string sessionid = OperationContext.Current.SessionId;
+            OperationContext.Current.Channel.Closing += new EventHandler(Channel_Closing); // Regist client's closing event
+
+            DicHostSessionid.Add(sessionid, client);//If type is seesion id, add it
+            DicHostUserid.Add(userName, sessionid);
+
+            isNewUser = true;
+        }
+
+        /// <summary>
+        /// Close client's event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Channel_Closing(object sender, EventArgs e)
+        {
+            if (DicHostSessionid != null && DicHostSessionid.Count > 0)
+            {
+                isNewUser = true;
+                foreach (var dic in DicHostUserid)
+                    if (DicHostSessionid[dic.Value] == (ICallBackServices)sender)//Remove the client's data
+                    {
+                        DicHostSessionid.Remove(DicHostUserid[dic.Key]);
+                        DicHostUserid.Remove(dic.Key);
+                        break;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// Decide who send and send to who
+        /// </summary>
+        /// <param name="person">Who send message</param>
+        /// <param name="specificClient">Send message to who</param>
+        public void SendToOtherClients(Person person, string specificClient)
+        {
+            SendMessenger = person;
+            ReceiveMessenger = specificClient;
+
+            isSendMessage = true;
+        }
+        /// <summary>
+        /// Get File data with type of list
+        /// </summary>
+        /// <param name="filePath">File path, ex:C:\\picDemo</param>
+        /// <returns>DataTable, include fileName, fileExtension, and fileSize</returns>
+        public DataSet GetFileList(string filePath)
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("FileName");
+            dataTable.Columns.Add("FileExtension");
+            dataTable.Columns.Add("FileSize");
+            DirectoryInfo directoryInfo = new DirectoryInfo(filePath); // Get path folder
+            foreach (FileSystemInfo fileSystemInfo in directoryInfo.GetFileSystemInfos())
+            {
+                FileInfo fileInfo = new FileInfo(fileSystemInfo.FullName); // Get file's information
+                DataRow dataRow = dataTable.NewRow();
+                dataRow["FileName"] = fileSystemInfo.FullName;
+                dataRow["FileExtension"] = fileSystemInfo.Extension;
+                dataRow["FileSize"] = fileInfo.Length;
+                dataTable.Rows.Add(dataRow);
+            }
+            DataSet dataSet = new DataSet();
+            dataSet.Tables.Add(dataTable);
+            return dataSet;
+        }
+
+        ///<summary>
+        /// Get picture Data
+        /// </summary> 
+        /// <param name="fileName">Downloading file name</param> 
+        /// <param name="offSet">Location</param> 
+        /// <param name="blockSize">Download data size per once</param> 
+        /// <returns>byte[], </returns> 
+        public byte[] GetPic(string filePath, string fileName, int offSet, int blockSize)
+        {
+            try
+            {
+                byte[] picByte;
+                using(FileStream fileStream = new FileStream(Path.Combine(@filePath, fileName),FileMode.Open,FileAccess.Read,FileShare.ReadWrite))
+                {
+                    fileStream.Seek(offSet, SeekOrigin.Begin);
+
+                    if ((offSet + blockSize) > fileStream.Length) // If asked download size is bigger than fileStream's size, reduce asked size.
+                        blockSize = Convert.ToInt32(fileStream.Length - offSet);
+                    if (blockSize <= 0) // If ask nothing, return 0
+                    {
+                        picByte = new byte[0];
+                        return picByte;
+                    }
+                    picByte = new byte[blockSize];
+                    fileStream.Read(picByte, 0, blockSize); // Read a block of bytes size (blocksize) from fileStream and writes the data into buffer (picByte)
+                    fileStream.Close(); // Close fileStream
+                }
+                    return picByte;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            } 
+        }
+
+
+        #region A way to avoid Application Configuration File
+        //public class MyHost
+        //{
+        //    static ServiceHost host = null;
+        //    public static void Open()
+        //    {
+        //        string baseAddress = "net.tcp://localhost:2008/FileService";
+        //        host = new ServiceHost(typeof(Service1), new Uri(baseAddress));
+        //        host.AddServiceEndpoint(typeof(IService1), GetTcpBinding(), "");
+        //        host.Open();
+        //    }
+        //    public static void Close()
+        //    {
+        //        if (host != null && host.State == CommunicationState.Opened)
+        //        {
+        //            host.Close();
+        //        }
+        //        host = null;
+        //    }
+        //    public static Binding GetTcpBinding()
+        //    {
+        //        NetTcpBinding binding = new NetTcpBinding();
+        //        binding.TransferMode = TransferMode.Streamed;
+        //        binding.MaxReceivedMessageSize = int.MaxValue;
+        //        return binding;
+        //    }
+        //}
+        #endregion
+
+
+
+        public void ReceiveFile(ClientFile clientFile, bool isChangeFileName)
+        {
+            if (clientFile.isFinsishFlag == false)
+            {
+                if (listenerHandler_ReceiveFile != null && isChangeFileName == false) // Check if this file is first time transport to Service then give it name
+                {
+                    listenerHandler_ReceiveFile(clientFile.ClientName, clientFile.FileName, isChangeFileName); // Make GUI display text
+                    _filePath = "D:\\Folder2\\" + clientFile.ClientName + "_" + clientFile.FileName;
+                    _filePath = CheckFileName(_filePath);
+
+                    fileStream = File.Open(_filePath, FileMode.Append);
+                }
+                fileStream.Write(clientFile.Buffer, 0, clientFile.BytesRead);
+            }
+            else
+            {
+                fileStream.Close();
+            }
+        }
+        private string CheckFileName(string filePath)
+        {
+            string[] splitPath = filePath.Split('.');
+            string extensionName = "." + splitPath[splitPath.Length - 1];
+            string _filePath = filePath;
+            int nameCount = 0;
+            while (true)
+            {
+                if (File.Exists(_filePath) == true)
+                {
+                    nameCount++;
+                    _filePath = filePath.Substring(0, filePath.Length - extensionName.Length) + "_" + nameCount.ToString() + extensionName;// - 1 for counting position
+                }
+                else
+                    return _filePath;
+            }
+        }
+    }
+}
---
+++ WCFServiceHostForm/ServiceForm.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceModel;
+using WCFService;
+using System.Threading;
+using System.IO;
+
+namespace WCFServiceHostForm//WCFServiceHostForm
+{
+    public partial class ServiceForm : Form
+    {
+        public ServiceForm()
+        {
+            InitializeComponent();
+        }
+        private static object InstObj = new object();
+        private int selectedFlag = 0;
+        private int selectedIndex = -1;
+        public Thread threadReceiveMessage;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServiceHost host = new ServiceHost(typeof(Service1));
+            host.Open(); // Start Listening
+            
+            Service1.listenerHandler_ReceiveFile += new Service1.ListenerHandler_ReceiveFile(ReceiveFile);
+
+            #region Thread Working
+
+            threadReceiveMessage = new Thread(new ThreadStart(delegate   //Listen all clients chat content
+            {
+                lock (InstObj)//Lock for multiple
+                {
+                    while (true)
+                    {
+                        if (Service1.isSendMessage == true) // Listen chat
+                        {
+                            SendToOtherClient(Service1.SendMessenger, Service1.ReceiveMessenger);
+                        }
+                        else if (Service1.isNewUser == true) // Listen login
+                        {
+                            AddNewUser();
+                        }
+                        //else if (Service1._isReceiveFile == true)
+                        //{
+                        //    ReceiveFile(Service1._clientFile);
+                        //}
+
+                        Thread.Sleep(10);
+                    }
+                }
+            }));
+            threadReceiveMessage.IsBackground = true;
+            threadReceiveMessage.Start();
+
+            #endregion
+        }
+        
+        private void AddNewUser()
+        {
+            string sessionID = "";
+            List<string> userList = new List<string>();
+            if (Service1.DicHostSessionid != null || Service1.DicHostSessionid.Count > 0)
+            {
+                this.Invoke(new MethodInvoker(delegate { this.lsbUserList.Items.Clear(); }));
+                foreach (var clientList in Service1.DicHostUserid) // id.key = userName, id.Value = sessionID
+                {
+                    userList.Add(clientList.Key);
+
+                    this.Invoke(new MethodInvoker(delegate
+                    {
+                        this.lsbUserList.Items.Add(clientList.Key); // UserList put in Service
+                    }));
+                }
+
+                foreach (var users in Service1.DicHostSessionid) // Update all client's list
+                {
+                    sessionID = users.Key;
+                    Service1.DicHostSessionid[sessionID].UpdateUserList(userList);
+                }
+            }
+            Service1.isNewUser = false; // Initialize
+        }
+
+        private void SendToOtherClient(Person person, string specificClient)
+        {
+            string sessionID = Service1.DicHostUserid[person.UserName];
+            if (specificClient != "")
+            {
+                if (specificClient == "Service")
+                {
+                    this.Invoke(new MethodInvoker(delegate
+                    {
+                        this.rtbHistory.SelectionColor = Color.Green;
+                        this.rtbHistory.AppendText(person.UserName + "[to you] : " + person.ChatContent + "\n");
+                        this.rtbHistory.SelectionColor = Color.White;
+                    }));
+                }
+                else 
+                {
+                    sessionID = Service1.DicHostUserid[specificClient];
+                    Service1.DicHostSessionid[sessionID].SendMessage(person, 3);
+                }
+            }
+            else
+            {
+                foreach (var id in Service1.DicHostSessionid) // Broadcast message to other clients without the one who send this message
+                {
+                    if (id.Key.ToString() != sessionID)
+                        id.Value.SendMessage(person, 2);
+                }
+
+                this.Invoke(new MethodInvoker(delegate
+                { 
+                    this.rtbHistory.AppendText(person.UserName + " : " + person.ChatContent + "\n"); 
+                }));
+            }
+            Service1.isSendMessage = false; // Initialize
+        }
+
+        private void ReceiveFile(string clientName, string fileName, bool isChangeFileName)
+        {
+            if (isChangeFileName == false) // Only active once
+            {
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    this.rtbHistory.SelectionColor = Color.Goldenrod;
+                    this.rtbHistory.AppendText(clientName + " transport a file \"" + fileName + "\" to you.\n");
+                    this.rtbHistory.SelectionColor = Color.White;
+                }));
+            }
+        }
+        
+        #region Toolbox Event
+        private void btnBroadCast_Click(object sender, EventArgs e)
+        {
+            Person person = new Person();
+            person.UserName = "Service";
+            person.ChatContent = this.txtChat.Text;
+            if (txtChat != null && lsbUserList.SelectedItem != null)
+            {
+                string selectedItem = lsbUserList.SelectedItem.ToString();
+                if (Service1.DicHostUserid.ContainsKey(selectedItem))
+                {
+                    string sessionID = Service1.DicHostUserid[selectedItem];
+                    Service1.DicHostSessionid[sessionID].SendMessage(person, 1); // Service to specific client
+
+                    rtbHistory.SelectionColor = Color.Red;
+                    rtbHistory.AppendText(person.UserName + "[to " + selectedItem + "] : " + person.ChatContent + "\n");
+                    rtbHistory.SelectionColor = Color.White;
+                }
+            }
+            else
+            {
+                foreach (var id in Service1.DicHostSessionid)
+                {
+                    id.Value.SendMessage(person, 0);// Broadcast message to every client
+                }
+                this.rtbHistory.AppendText(person.UserName + " : " + person.ChatContent + "\n"); // Add text into ChatBoard
+            }
+            
+        }
+
+        private void lsbUserList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lsbUserList.SelectedIndex == selectedIndex && selectedFlag == 1)
+            {
+                lsbUserList.SelectedIndex = -1;
+                selectedFlag = 0;
+            }
+            else
+            {
+                selectedIndex = lsbUserList.SelectedIndex;
+                selectedFlag = 1;
+            }
+        }
+        #endregion
+    }
+}
